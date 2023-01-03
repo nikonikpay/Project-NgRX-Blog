568a928c2c906b202d0577ca7d53aaa69ec6d7c1
nikonikpay <nikonikpay@gmail.com>
2023-01-04 00:13:41 +0100

    the completed app

---
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -0,0 +1,84 @@
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core'
+import {ActivatedRoute, Params, Router} from '@angular/router'
+import {select, Store} from '@ngrx/store'
+import queryString from 'query-string';
+
+import {Observable, Subscription} from 'rxjs'
+
+
+import {getFeedAction} from 'src/app/shared/modules/feed/store/actions/getFeed.action'
+import {errorSelector, feedSelector, isLoadingSelector} from 'src/app/shared/modules/feed/store/selectors'
+import {GetFeedResponseInterface} from 'src/app/shared/modules/feed/types/getFeedResponse.interface'
+import {environment} from 'src/environments/environment'
+
+@Component({
+             selector   : 'mc-feed',
+             templateUrl: './feed.component.html',
+             styleUrls  : ['./feed.component.scss']
+           })
+export class FeedComponent implements OnInit, OnDestroy, OnChanges {
+  @Input('apiUrl') apiUrlProps: string
+
+  feed$: Observable<GetFeedResponseInterface | null>
+  error$: Observable<string | null>
+  isLoading$: Observable<boolean>
+  limit = environment.limit
+  baseUrl: string
+  queryParamsSubscription: Subscription
+  currentPage: number
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    console.log('initialize feed', this.apiUrlProps)
+    this.initializeValues()
+    this.initializeListeners()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const isApiUrlChanged =
+            !changes['apiUrlProps'].firstChange &&
+            changes['apiUrlProps'].currentValue !== changes['apiUrlProps'].previousValue
+    if (isApiUrlChanged) {
+      this.fetchFeed()
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe()
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params['page'] || '1')
+        console.log('fetchFeed')
+        this.fetchFeed()
+      }
+    )
+  }
+
+  initializeValues(): void {
+    this.feed$ = this.store.pipe(select(feedSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.baseUrl = this.router.url.split('?')[0]
+  }
+
+  fetchFeed(): void {
+    const offset = this.currentPage * this.limit - this.limit
+    const parsedUrl = queryString.parseUrl(this.apiUrlProps)
+    const stringifiedParams = queryString.stringify({
+                                                      limit: this.limit,
+                                                      offset,
+                                                      ...parsedUrl.query
+                                                    })
+    const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
+    this.store.dispatch(getFeedAction({url: apiUrlWithParams}))
+  }
+}
